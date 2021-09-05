a332faf3ee668dcb7cb70dd9c1ff389b0bf991af
Jagadeesh77 <venkat.jagadeesh77@gmail.com>
2021-09-05 13:17:46 +0530

    Initial commit

---
+++ src/main/java/com/oosd/vstudent/VstudentApplication.java
@@ -0,0 +1,13 @@
+package com.oosd.vstudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VstudentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VstudentApplication.class, args);
+    }
+
+}

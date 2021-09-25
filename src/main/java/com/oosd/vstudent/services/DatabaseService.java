5c77679af4ccb703e924a4bbfe836a58c4a74bfb
Jagadeesh77 <venkat.jagadeesh77@gmail.com>
2021-09-25 18:58:38 +0530

    added a service layer

---
+++ src/main/java/com/oosd/vstudent/services/DatabaseService.java
@@ -0,0 +1,59 @@
+package com.oosd.vstudent.services;
+
+import com.oosd.vstudent.models.Comment;
+import com.oosd.vstudent.repositories.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DatabaseService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Autowired
+    private TagRepository tagRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private DocumentRepository documentRepository;
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Autowired
+    private CarPoolRepository carPoolRepository;
+
+    public StudentRepository getStudentRepository() {
+        return studentRepository;
+    }
+
+    public TagRepository getTagRepository() {
+        return tagRepository;
+    }
+
+    public RoleRepository getRoleRepository() {
+        return roleRepository;
+    }
+
+    public PostRepository getPostRepository() {
+        return postRepository;
+    }
+
+    public DocumentRepository getDocumentRepository() {
+        return documentRepository;
+    }
+
+    public CommentRepository getCommentRepository() {
+        return commentRepository;
+    }
+
+    public CarPoolRepository getCarPoolRepository() {
+        return carPoolRepository;
+    }
+}

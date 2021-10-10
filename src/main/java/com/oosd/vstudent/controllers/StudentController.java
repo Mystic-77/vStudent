4d917be675588d5e10f084a8b89075e64bfc17bd
Jagadeesh77 <venkat.jagadeesh77@gmail.com>
2021-10-10 21:17:57 +0530

    added student controller

--- src/main/java/com/oosd/vstudent/controllers/StudentController.java
+++ src/main/java/com/oosd/vstudent/controllers/StudentController.java
@@ -1,11 +1,13 @@
 package com.oosd.vstudent.controllers;
 
+import com.oosd.vstudent.errors.InvalidEndpointException;
+import com.oosd.vstudent.errors.SuccessResponse;
 import com.oosd.vstudent.models.Student;
 import com.oosd.vstudent.services.DatabaseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +22,57 @@ public class StudentController {
     public List<Student> retreiveAllStudents()
     {
         return databaseService.getStudentRepository().findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Student retrieveStudent(@PathVariable int id)
+    {
+        if (!databaseService.getStudentRepository().existsById(id))
+        {
+            throw new InvalidEndpointException("id not found");
+        }
+        return databaseService.getStudentRepository().getById(id);
+    }
+
+    @PostMapping("/")
+    public Student addStudent(@RequestBody Student student)
+    {
+        return databaseService.getStudentRepository().save(student);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<SuccessResponse> editStudent(@PathVariable int id, @RequestBody Student student)
+    {
+        if (!databaseService.getStudentRepository().existsById(id))
+        {
+            throw new InvalidEndpointException("id not found");
+        }
+        Student s = databaseService.getStudentRepository().getById(id);
+        if (student.getEmail() != null) { s.setEmail(student.getEmail()); }
+        if (student.getUsername() != null) { s.setUsername(student.getUsername()); }
+        if (student.getPassword() != null) { s.setPassword(student.getPassword()); }
+
+        databaseService.getStudentRepository().save(s);
+
+        SuccessResponse sr = new SuccessResponse();
+        sr.setMessage("fields edited");
+        sr.setStatus(HttpStatus.OK.value());
+
+        return new ResponseEntity<SuccessResponse>(sr, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<SuccessResponse> deleteStudent(@PathVariable int id)
+    {
+        if (!databaseService.getStudentRepository().existsById(id))
+        {
+            throw new InvalidEndpointException("id not found");
+        }
+
+        SuccessResponse sr = new SuccessResponse();
+        sr.setMessage("student deleted");
+        sr.setStatus(HttpStatus.OK.value());
+
+        return new ResponseEntity<SuccessResponse>(sr, HttpStatus.OK);
     }
 }
